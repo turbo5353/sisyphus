2a9082302b5b7a850606009fc53fdfd6cec5ceee
Tobias Wienkoop <67444796+turbo5353@users.noreply.github.com>
2021-09-13 00:10:43 +0200

    Start syncing the x in the task string with the check button

--- sisyphus.c
+++ sisyphus.c
@@ -18,6 +18,7 @@ void set_task_str(Task *task, char *str) {
     size_t len = strlen(str);
     if (len > task->task_len) {
         task->task_str = (char*) realloc(task->task_str, len * sizeof(char));
+        task->task_len = len;
     }
 
     strcpy(task->task_str, str);
@@ -25,10 +26,18 @@ void set_task_str(Task *task, char *str) {
 
 GtkWidget* create_task_element(Task task) {
     GtkWidget *task_element = gtk_check_button_new();
+
+    if (strlen(task.task_str) >= 2) {
+        if (strncmp(task.task_str, "x ", strlen("x ")) == 0) {
+            gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(task_element), TRUE);
+        }
+    }
+
     gchar *escaped = g_markup_escape_text(task.task_str, -1);
     GtkWidget *label = gtk_label_new(escaped);
     g_free(escaped);
     gtk_label_set_use_markup(GTK_LABEL(label), TRUE);
+
     gtk_container_add(GTK_CONTAINER(task_element), label);
     return task_element;
 }
@@ -43,21 +52,50 @@ void allocate_task_list(unsigned int num_tasks) {
     }
 }
 
-static void build_ui(GtkApplication *app) {
-    // Create window
-    GtkWidget *window = gtk_application_window_new(app);
+void task_toggled(GtkWidget *check, gpointer data);
 
-    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 6);
-    gtk_widget_set_margin_top(box, 12);
-    gtk_widget_set_margin_bottom(box, 12);
-    gtk_widget_set_margin_start(box, 12);
-    gtk_widget_set_margin_end(box, 12);
-    gtk_container_add(GTK_CONTAINER(window), box);
+GtkWidget *task_box = NULL;
+void create_task_ui(void) {
+    // Remove all previous children
+    GList *list = NULL, *iterator = NULL;
+    list = gtk_container_get_children(GTK_CONTAINER(task_box));
+    for (iterator = list; iterator; iterator = iterator->next) {
+        gtk_container_remove(GTK_CONTAINER(task_box), GTK_WIDGET(iterator->data));
+    }
 
     for (unsigned int i = 0; i < g_num_tasks; i++) {
         GtkWidget *check = create_task_element(task_list[i]);
-        gtk_container_add(GTK_CONTAINER(box), check);
+        g_signal_connect(check, "toggled", G_CALLBACK(task_toggled), (void*)(task_list + i));
+        gtk_container_add(GTK_CONTAINER(task_box), check);
     }
+
+    gtk_widget_show_all(task_box);
+}
+
+void task_toggled(GtkWidget *check, gpointer data) {
+    Task *task = (Task*) data;
+
+    if (strlen(task->task_str) >= 2) {
+        if (strncmp(task->task_str, "x ", strlen("x ")) == 0) {
+            memmove(task->task_str, task->task_str + 2, strlen(task->task_str) - 2);
+        }
+    }
+
+    create_task_ui();
+}
+
+void build_ui(GtkApplication *app) {
+    // Create window
+    GtkWidget *window = gtk_application_window_new(app);
+
+    task_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 6);
+    gtk_widget_set_margin_top(task_box, 12);
+    gtk_widget_set_margin_bottom(task_box, 12);
+    gtk_widget_set_margin_start(task_box, 12);
+    gtk_widget_set_margin_end(task_box, 12);
+    gtk_container_add(GTK_CONTAINER(window), task_box);
+
+    create_task_ui();
 
     // Show the window and all its children
     gtk_widget_show_all(GTK_WIDGET(window));
